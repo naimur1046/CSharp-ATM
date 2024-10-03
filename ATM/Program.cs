18c2e5fb51805678b2ffa00d947d3d9e9ee40b83
naimur rahman rumel <naimurrahman1046@gmail.com>
2024-10-03 17:16:05 +0600

    In this section I have completed the main functionality

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class cardHolder
+{
+    private String cardNumber;
+    private String firstName;
+    private String lastName;
+    private double balance;
+    int pin;
+    public cardHolder(String cardNumber, String firstName, String lastName, double balance,int pin) {
+        
+        this.cardNumber = cardNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+        this.pin = pin;
+    }
+    public String getCardNumber()
+    {
+        return cardNumber;
+    }
+    public int getPin()
+    {
+        return pin;
+    }
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public String getLastName()
+    {
+        return lastName;
+    }
+    public double getBalance()
+    {
+        return balance;
+    }
+    public void setBalance(double balance)
+    {
+        this.balance = balance;
+    }
+    public void setFirstName(String name)
+    {
+        this.firstName = name;
+    }
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    } 
+
+    public static void Main(string[] args)
+    {
+        void printOptions()
+        {
+            Console.WriteLine("");
+        }
+    }
+    
+}
